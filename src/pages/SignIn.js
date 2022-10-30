3e3cc2b25a8bba2ee6e2c4446fc22047e32dc74d
diegocatalan8 <diegocatalan.sdev@gmail.com>
2022-10-30 15:32:56 -0600

    Correccion de ruta en SignIn

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -47,7 +47,7 @@ export default function SignIn() {
 
 //peticion para pedir el usuario
 const userLoad = async ()=>{
-    const response = await fetch("https://lcloi-gt.vercel.app/api/user");
+    const response = await fetch("https://cloi-gt.vercel.app/api/user");
     const usuario = await response.json();
 
     setUserValidation(usuario);
