ea269baf69a11c78c5b7b9f37e983f9941e77bd4
diegocatalan8 <diegocatalan.sdev@gmail.com>
2022-10-30 18:24:25 -0600

    CLOI-GT V.1.3.7

--- src/middleware.js
+++ src/middleware.js
@@ -25,5 +25,5 @@ export  async function middleware(request) {
 }
 
 export const config = {
-    matcher: [ '/Loading', '/LoadingHome', '/Restaurantes', '/' ]
+    matcher: ['/Home', '/Loading', '/LoadingHome', '/Restaurantes', '/' ]
 }
